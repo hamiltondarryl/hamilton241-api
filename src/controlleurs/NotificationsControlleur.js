b101e324bc8e520b836b0bc9c958923c1cc6bcdc
hamilto ndarryl <hamiltondarryl24@gmail.com>
2022-01-25 13:36:18 +0100

    envoi

--- src/controlleurs/NotificationsControlleur.js
+++ src/controlleurs/NotificationsControlleur.js
@@ -4,36 +4,43 @@ const serviceNotification = require('../helpers/services');
 exports.touslesutilisateurs = (req, res )=>{
 
 
-    let { key, appid , titre , contenu  } = req.body ;
+    let { key, appid , titre , contenu , urlimage } = req.body ;
 
-    var message = {
-        app_id : appid,
-        headings : {en : titre },
-        contents : { en: contenu },
-        included_segments : ["All"],
-        content_available : true,
-        small_icon: "ic_notification_icon",
-        big_picture : 'https://img.cuisineaz.com/680x357/2016/07/25/i18826-tbikha-aubergine.jpg',
-        chrome_web_image : 'https://serveur-sodepsi.com/CRYSTAL/assets/img/uploads/plat1.jpg',
-        huawei_big_picture : 'https://www.mutuellebleue.fr/conseils-sante-bien-etre/wp-content/uploads/sites/2/2020/07/des-plats-%C3%A9quiliobr%C3%A9s-et-color%C3%A9s.jpg',
-        data : {
-            "PushTitle" : "Custom notifications"
-        }
-    }
+    if (key !="" && appid !="" && titre !="" && contenu !="") {
 
-    serviceNotification.sendAll(message, key, (errorr, result) => {
-
-        if (errorr) {
-            res.status(400).json({ resultat : false , titre : "Probléme décelé", "message" : errorr })
-        } else {
-            if (result.errors) {
-                res.status(400).json({ resultat : false , titre : "Erreur d'envoie", "message" : `${result.errors}` })
-            }else{
-                res.status(200).json({ resultat : true , titre : "Envoie reussi", "message" : "Les notifications ont été envoyé avec success"  })
+        var message = {
+            app_id : appid,
+            headings : {en : titre },
+            contents : { en: contenu },
+            included_segments : ["All"],
+            content_available : true,
+            small_icon: "ic_notification_icon",
+            big_picture : urlimage,
+            data : {
+                "PushTitle" : "Custom notifications"
             }
         }
-      
-    });
+    
+        serviceNotification.sendAll(message, key, (errorr, result) => {
+    
+            if (errorr) {
+                res.status(400).json({ resultat : false , titre : "Probléme décelé", "message" : errorr })
+            } else {
+                if (result.errors) {
+                    res.status(400).json({ resultat : false , titre : "Erreur d'envoie", "message" : `${result.errors}` })
+                }else{
+                    res.status(200).json({ resultat : true , titre : "Envoie reussi", "message" : "Les notifications ont été envoyé avec success"  })
+                }
+            }
+          
+        });
+        
+    }else{
+
+        res.status(400).json({ resultat : false , titre : "Il y'a des champs vides", "message" : `Veuillez remplir tous les champs pour que la requette puisse etre prise en charge`})
+    }
+
+    
 
 
 
