ec8704fb767dab3499901c0749fe468da5406a3b
hamilto ndarryl <hamiltondarryl24@gmail.com>
2022-01-25 16:12:23 +0100

    ajout des notifications par token

--- src/controlleurs/NotificationsControlleur.js
+++ src/controlleurs/NotificationsControlleur.js
@@ -3,7 +3,6 @@ const serviceNotification = require('../helpers/services');
 // Fonction d'envoie des notifications à tous les utilisateur
 exports.touslesutilisateurs = (req, res )=>{
 
-
     let { key, appid , titre , contenu , urlimage } = req.body ;
 
     if (key !="" && appid !="" && titre !="" && contenu !="") {
@@ -39,8 +38,46 @@ exports.touslesutilisateurs = (req, res )=>{
 
         res.status(400).json({ resultat : false , titre : "Il y'a des champs vides", "message" : `Veuillez remplir tous les champs pour que la requette puisse etre prise en charge`})
     }
+}
 
+exports.specificutilisateurs = (req, res )=>{
+
+    let { key, appid , titre , contenu , urlimage, ids } = req.body ;
+
+
+    if (key !="" && appid !="" && titre !="" && contenu !="") {
+
+        var message = {
+            app_id : appid,
+            headings : {en : titre },
+            contents : { en: contenu },
+            include_player_ids : ids ,
+            content_available : true,
+            small_icon: "ic_notification_icon",
+            big_picture : urlimage,
+            data : {
+                "PushTitle" : "Custom notifications"
+            }
+        }
     
+        serviceNotification.sendAll(message, key, (errorr, result) => {
+    
+            if (errorr) {
+                res.status(400).json({ resultat : false , titre : "Probléme décelé", "message" : errorr })
+            } else {
+                if (result.errors) {
+                    res.status(400).json({ resultat : false , titre : "Erreur d'envoie", "message" : `${result.errors}` })
+                }else{
+                    res.status(200).json({ resultat : true , titre : "Envoie reussi", "message" : "Les notifications ont été envoyé avec success"  })
+                }
+            }
+          
+        });
+        
+    }else{
+
+        res.status(400).json({ resultat : false , titre : "Il y'a des champs vides", "message" : `Veuillez remplir tous les champs pour que la requette puisse etre prise en charge`})
+    }
 
 
 
--- src/routes/NotificationsRouter.js
+++ src/routes/NotificationsRouter.js
@@ -5,4 +5,7 @@ const controller = require('../controlleurs/NotificationsControlleur');
 // Envoie de la notification à tous les utilisateurs 
 router.post("/tous-les-utilisateurs", controller.touslesutilisateurs);
 
+// Envoie de la notification à des utilisateurs bien defini par token
+router.post("/specifiques-utilisateurs", controller.specificutilisateurs);
+
 module.exports = router ;
