e6c714a2428046e9730a13b827fbeffd65d6fa4b
hamilto ndarryl <hamiltondarryl24@gmail.com>
2022-01-25 10:02:22 +0100

    envoi

--- src/controlleurs/NotificationsControlleur.js
+++ src/controlleurs/NotificationsControlleur.js
@@ -3,8 +3,17 @@ const serviceNotification = require('../helpers/services');
 // Fonction d'envoie des notifications à tous les utilisateur
 exports.touslesutilisateurs = (req, res )=>{
 
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
 
     res.send("ok gar")
 
