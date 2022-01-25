d8197efae0d44c811a4720ded6b8bf664fe7f012
hamilto ndarryl <hamiltondarryl24@gmail.com>
2022-01-25 12:12:55 +0100

    envoi

--- src/app.js
+++ src/app.js
@@ -11,12 +11,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/notifications', notificationRouter);
 
-app.all('/', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-   });
-
 // Page 404
 app.get('*', function(req, res){
     res.status(404).send('<h1 style="text-align : center ">PAGE INTROUVABLE </h1>');
